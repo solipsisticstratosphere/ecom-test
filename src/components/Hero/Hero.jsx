a37ad0bdaa4c6b75678acfe6967b0afd93ff5e67
Yaroslav Klimenko <klimyarik13@gmail.com>
2025-06-15 21:28:21 +0300

    hero some ui upcales

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,22 +10,38 @@ const Hero = () => {
           <h1>
             BUILDING <span>DATA-DRIVEN</span> E-COMMERCE <br /> ON SHOPIFY
           </h1>
-          <p>
-            We help businesses grow and scale with our expertise in{" "}
-            <span className={styles.tag}>performance marketing</span>,{" "}
-            <span className={styles.tag}>user experience</span> and{" "}
-            <span className={styles.tag}>creative strategy</span>.
-          </p>
-          <button className={styles.cta}>
-            FREE STRATEGY CALL
-            <span className={styles.icon}>
-              <CalendarIcon />
-            </span>
-          </button>
-          <p className={styles.reviews}>
-            <strong>146+ REVIEWS.</strong> ALL CHANCES ARE YOU’LL BE IMPRESSED
-            TOO.
-          </p>
+          <div className={styles.descriptionBox}>
+            <p className={styles.description}>
+              We help businesses grow and scale
+              <br /> with our expertise in{" "}
+              <span className={styles.tagFirst}>
+                <span className={styles.tagText}>performance </span>{" "}
+                <span className={styles.tagSpan}> marketing</span>
+              </span>{" "}
+              ,
+              <span className={styles.tag}>
+                <span className={styles.tagText}>user </span>{" "}
+                <span className={styles.tagSpan}>experience</span>
+              </span>{" "}
+              and{" "}
+              <span className={styles.tag}>
+                <span className={styles.tagText}> creative </span>{" "}
+                <span className={styles.tagSpan}>strategy</span>
+              </span>
+            </p>
+            <div className={styles.ctaBox}>
+              <button className={styles.cta}>
+                FREE STRATEGY CALL
+                <span className={styles.icon}>
+                  <CalendarIcon />
+                </span>
+              </button>
+              <p className={styles.reviews}>
+                <strong>146+ REVIEWS.</strong> ALL CHANCES ARE YOU’LL BE
+                IMPRESSED TOO.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
